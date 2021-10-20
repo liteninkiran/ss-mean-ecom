32e89b809d099495998c1d9be9635f3458c4e962
Kiran Anand <liteninkiran@hotmail.com>
2021-10-20 22:27:56 +0100

    Get one category

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -10,6 +10,14 @@ router.get('/', async (req, res) => {
     res.send(categories);
 });
 
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+        res.status(500).json({ success: false, message: 'Category not found' });
+    }
+    res.send(category);
+});
+
 router.post('/', async (req, res) => {
     let category = new Category({
         name: req.body.name,
@@ -37,7 +45,7 @@ router.delete('/:id', async (req, res) => {
         if (category) {
             return res.status(200).json({ success: true, message: 'Category deleted successfully' });
         } else {
-            return res.status(404).json({ success: false, message: 'Could not find category' });
+            return res.status(404).json({ success: false, message: 'Category not found' });
         }
     }).catch(err => {
         return res.status(400).json({ success: false, err: err });
