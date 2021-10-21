d72ce4d212c1bfd65e5409f10213ae344d4bc1f4
Kiran Anand <liteninkiran@hotmail.com>
2021-10-21 19:28:22 +0100

    Update product

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -4,11 +4,48 @@ const { Product } = require('../models/product');
 const { Category } = require('../models/category');
 
 router.get('/', async (req, res) => {
-    const products = await Product.find();
+    const products = await Product.find(); // .select('name image -_id');
     if (!products) {
         res.status(500).json({ success: false });
     }
     res.send(products);
+});
+
+router.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        res.status(500).json({ success: false, message: 'Product not found' });
+    }
+    res.send(product);
+});
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findById(req.body.category);
+
+    if (!category) {
+        return res.status(400).send('Invalid category');
+    }
+
+    const product = await Product.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: category.id,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    }, {
+        new: true,
+    });
+    if (!product) {
+        res.status(500).json({ success: false, message: 'Product not found' });
+    }
+    res.send(product);
 });
 
 router.post('/', async (req, res) => {
