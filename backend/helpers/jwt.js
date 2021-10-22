5601289fed3bcdac367e3383bb758c1296e429c9
Kiran Anand <liteninkiran@hotmail.com>
2021-10-22 12:30:29 +0100

    Using regex in middleware

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -11,7 +11,8 @@ function authJwt() {
         path: [
             `${api}/users/login`,
             `${api}/users/register`,
-            { url: `${api}/products`, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
         ]
     });
 }
