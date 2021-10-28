a73f4e862d1dc3c96994e262057f6abfea0b2273
Kiran Anand <liteninkiran@hotmail.com>
2021-10-28 13:45:43 +0100

    Exclude uploads from authentication

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -12,6 +12,7 @@ function authJwt() {
         path: [
             `${api}/users/login`,
             `${api}/users/register`,
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST']},
