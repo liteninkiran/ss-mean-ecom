cda873c70d2d8b0305d46a3eb0954724eb6ea05c
Kiran Anand <liteninkiran@hotmail.com>
2021-10-28 22:24:15 +0100

    Add header & footer

--- frontend/apps/ng-shop/src/app/shared/footer/footer.component.ts
+++ frontend/apps/ng-shop/src/app/shared/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'bluebits-footer',
+  selector: 'ng-shop-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
--- frontend/apps/ng-shop/src/app/shared/header/header.component.ts
+++ frontend/apps/ng-shop/src/app/shared/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'bluebits-header',
+  selector: 'ng-shop-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
