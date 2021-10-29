2c42d7a00a9f20c996dad47cfe46f8e2b576d451
Kiran Anand <liteninkiran@hotmail.com>
2021-10-29 16:02:41 +0100

    Implement banner

--- frontend/apps/ng-shop/src/app/app.module.ts
+++ frontend/apps/ng-shop/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { UiModule } from '@bluebits/ui';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -14,8 +15,18 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-    imports: [BrowserModule, RouterModule.forRoot(routes)],
+    declarations: [
+        AppComponent,
+        HomePageComponent,
+        ProductListComponent,
+        HeaderComponent,
+        FooterComponent,
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes),
+        UiModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
