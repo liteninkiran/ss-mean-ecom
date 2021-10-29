8ee033bca20fbdc2b92f9397200c5e10aa503aad
Kiran Anand <liteninkiran@hotmail.com>
2021-10-29 15:54:11 +0100

    Slider component
    npx ng generate @schematics/angular:component --name=slider --project=ui --style=scss --export --prefix=ui --selector=slider --no-interactive

--- frontend/libs/ui/src/lib/ui.module.ts
+++ frontend/libs/ui/src/lib/ui.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
-    BannerComponent
+    BannerComponent,
+    SliderComponent
   ],
   exports: [
-    BannerComponent
+    BannerComponent,
+    SliderComponent
   ],
 })
 export class UiModule {}
